c232ffb7028b93d80993e09bdf009b41dc80d603
Alessandro Ricci <ricci.ghost.ale@gmail.com>
2025-02-05 23:20:40 +0100

    feat(Auth): Adding Authentication Controller, refactor some logics

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,14 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Requests\AuthRequest;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('/auth', function (AuthRequest $request) {
 
-    // The incoming request is valid... AuthRequest do the trick
-
-    if (!Auth::attempt($request->validated())) {
-        return response()->json([
-            'error' => 'Unauthenticated',
-            'message' => 'Invalid token or credentials'
-        ], 401);
-    }
-
-    $user = User::where("email", $request->validated("email"))->first();
-    return response()->json([
-        'token' => $user->createToken('TokenApi')->plainTextToken
-    ]);
-});
-
+Route::post('/auth', [AuthenticationController::class, 'Auth']);
 // A route named "login" is necessary to handle sanctum redirect behaviour on failed authentication
-
-Route::get('/auth-failed', function (Request $request) {
-
-    return response()->json([
-        'error' => 'Unauthenticated',
-        'message' => 'Invalid token or credentials'
-    ], 401);
-})->name("login");
+Route::get('/auth-failed', [AuthenticationController::class, "unauthorizedResponse"])->name("login");
