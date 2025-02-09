386523c8c8fae98c56017ae3ff340ee795a18a0a
Alessandro Ricci <ricci.ghost.ale@gmail.com>
2025-02-09 18:22:04 +0100

    Service(Order): Adding OrderController & routing

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,12 @@ Route::get('/user', function (Request $request) {
 Route::post('/auth', [AuthenticationController::class, 'Auth']);
 // A route named "login" is necessary to handle sanctum redirect behaviour on failed authentication
 Route::get('/auth-failed', [AuthenticationController::class, "unauthorizedResponse"])->name("login");
+
+
+Route::prefix('order')->middleware(['auth:api'])->group(function () {
+    Route::get('/', [OrderController::class, "index"]);
+    Route::post('/', [OrderController::class, "store"]);
+    Route::get('/{id}', [OrderController::class, "show"]);
+    Route::patch('/{id}', [OrderController::class, 'update']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+});
