2fd16abac0a82769a47e61d49ba958d6f4c6f765
Alessandro Ricci <ricci.ghost.ale@gmail.com>
2025-02-09 18:19:45 +0100

    Service(Order): Creating request validators

---
+++ app/Http/Requests/OrderUpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if ($this->user()->role === "USER") {
+            return true;
+        };
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'status' => 'required|in:DRAFT,PAID,SHIPPED,COMPLETED',
+            'products.*.id' => 'required|integer|exists:products,id',
+            'products.*.qty' => 'required|integer'
+        ];
+    }
+}
